6e613ba89220df4962675e71e086d9770073f585
Joel Bakken <jkbetc@gmail.com>
2017-09-05 16:15:53 -0700

    add code to initialize ListView and set it using .findViewById method

--- app/src/main/java/com/example/joelbakken/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/joelbakken/myrestaurants/RestaurantsActivity.java
@@ -3,21 +3,24 @@ package com.example.joelbakken.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+    private ListView mListView;
     private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
             "Chipotle", "Subway"};
-    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         Intent intent = getIntent();
