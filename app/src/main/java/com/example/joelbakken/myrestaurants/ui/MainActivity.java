c01b19de5fcab73336fe49c0a96837cadba28acd
Guest User <Guest@epicodus-53.local>
2017-09-21 09:08:48 -0700

    lesson follow along adding firebase to the application

--- app/src/main/java/com/example/joelbakken/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/joelbakken/myrestaurants/ui/MainActivity.java
@@ -42,7 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
-            addToSharedPreferences(location);
+            if(!(location).equals("")) {
+                addToSharedPreferences(location);
+            }
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
             startActivity(intent);
         }
--- app/src/main/java/com/example/joelbakken/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/example/joelbakken/myrestaurants/ui/RestaurantListActivity.java
@@ -35,7 +35,7 @@ public class RestaurantListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant);
+        setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
