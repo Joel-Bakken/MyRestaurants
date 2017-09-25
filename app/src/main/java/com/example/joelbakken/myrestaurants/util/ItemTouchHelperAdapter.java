935113e60bc2938e0aa17238694ab4709be6efd4
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-25 08:43:32 -0700

    add a new subclass and two new java classes to said subclass plus add a new icon to reorder list items

---
+++ app/src/main/java/com/example/joelbakken/myrestaurants/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,8 @@
+package com.example.joelbakken.myrestaurants.util;
+
+
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/example/joelbakken/myrestaurants/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.example.joelbakken.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Guest on 9/25/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
